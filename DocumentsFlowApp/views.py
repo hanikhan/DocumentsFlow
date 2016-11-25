55fe50684c3d7e84c13a2f7a857151584af663c3
patriciamazere <patriciamazere@gmail.com>
2016-11-25 19:01:29 +0200

    Add DocumentsFlowApp directory

---
+++ DocumentsFlowApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the DocumentsFlowApp index.")
